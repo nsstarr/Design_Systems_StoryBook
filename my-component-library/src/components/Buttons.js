e859577ac0f87f43d67e56770ded6d5126d3000a
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-10-05 14:26:17 +0100

    add primary, secondary and tertiary buttons with styled components

--- my-component-library/src/components/Buttons.js
+++ my-component-library/src/components/Buttons.js
@@ -1,12 +1,31 @@
 import styled from "styled-components";
 
-const PrimaryButton = styled.button`
-  background-color: red;
-  border: none;
-  color: white;
-  padding: 12px 24px;
+export const primaryGreen = "#4B5229";
+
+const Button = styled.button`
+  padding: 8px 12px;
   font-size: 1rem;
+  border-radius: 2px;
+  min-width: 100px;
+  cursor: pointer;
+  font-family: "Indie Flower", cursive;
+  border-radius: 19px;
+  color: white;
 `;
 
-export default PrimaryButton;
+export const PrimaryButton = styled(Button)`
+  background-color: #4b5229;
+  border: none;
+`;
 
+export const SecondaryButton = styled(Button)`
+  border: 1px solid #4b5229;
+  background-color: transparent;
+  color: #4b5229;
+`;
+
+export const TertiaryButton = styled(Button)`
+  border: 2px solid transparent;
+  background-color: transparent;
+  color: #4b5229;
+`;
--- my-component-library/src/index.js
+++ my-component-library/src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PrimaryButton from "./components/Buttons";
+import {
+  PrimaryButton,
+  SecondaryButton,
+  TertiaryButton,
+} from "./components/Buttons";
 
-const App = () => <PrimaryButton>Hello world</PrimaryButton>;
+const App = () => {
+  return (
+    <>
+      <PrimaryButton>Primary Button</PrimaryButton>
+      <SecondaryButton>Secondary Button</SecondaryButton>
+      <TertiaryButton>Tertiary Button</TertiaryButton>
+    </>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
