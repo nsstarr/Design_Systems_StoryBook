e19281dcc2df399d501b0f0b313fa69f60512b07
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-10-12 22:18:09 +0100

    finish storybook-build

---
+++ my-component-library/storybook-static/precache-manifest.c27a56c5cf48f5ac0b118b1b9347fb6a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf29b59df4f5e0eb8b20",
+    "url": "0.3ee60dfe.iframe.bundle.js"
+  },
+  {
+    "revision": "6db76acb3ac2b5e52448",
+    "url": "1.8b4075bd.iframe.bundle.js"
+  },
+  {
+    "revision": "7188010768ed87a8a179",
+    "url": "2.6a1e9316.iframe.bundle.js"
+  },
+  {
+    "revision": "841a81a8c4644d6032eb",
+    "url": "static/css/main.db879700.chunk.css"
+  },
+  {
+    "revision": "841a81a8c4644d6032eb",
+    "url": "main.28ae0acc.iframe.bundle.js"
+  },
+  {
+    "revision": "9fb9972248f9978c9452",
+    "url": "runtime~main.5982efe9.iframe.bundle.js"
+  },
+  {
+    "revision": "ad504b45c6570c8f4bcf",
+    "url": "vendors~main.a69a10df.iframe.bundle.js"
+  },
+  {
+    "revision": "fabb08257b9d7c300764",
+    "url": "6.ce8d773b.iframe.bundle.js"
+  },
+  {
+    "revision": "ea039c13a443efd9c9b0",
+    "url": "7.82c81f44.iframe.bundle.js"
+  },
+  {
+    "revision": "88b2cee5058d4c9131cc",
+    "url": "8.10634b2a.iframe.bundle.js"
+  },
+  {
+    "revision": "2fb9335f3a2d6a7a5a54",
+    "url": "9.f707eee6.iframe.bundle.js"
+  },
+  {
+    "revision": "3af7d3dc5af69a79a92a",
+    "url": "10.2424183d.iframe.bundle.js"
+  },
+  {
+    "revision": "475e8713bdba5b0e6285",
+    "url": "11.8aaac3a8.iframe.bundle.js"
+  },
+  {
+    "revision": "f78716418ec8337e321b508175e5c0f4",
+    "url": "static/media/SignUp.f7871641.svg"
+  },
+  {
+    "revision": "a252366e437000c59bbde81a9807bcdf",
+    "url": "0.3ee60dfe.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "34cb9702a7d19f940e5dfc26cffd83b4",
+    "url": "10.2424183d.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e968d796aa5cd7a396045e69ecde11",
+    "url": "6.ce8d773b.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "9.f707eee6.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "77575f73ce0e269b0a2abe85f0f1abe8",
+    "url": "vendors~main.a69a10df.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b5bcb11b7063a8d0876a288daa6912",
+    "url": "iframe.html"
+  }
+];
